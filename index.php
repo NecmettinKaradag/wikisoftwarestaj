ecef272560b0e72a118bd53a06b2cc955d4670b5
NecmettinKaradag <necmettink19@gmail.com>
2019-12-28 22:30:15 +0300

    project pertema

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+echo '{"list":[
+  {
+    "id": 11,
+    "title": "Hangi takımı tutuyorsunuz?",
+    "type": "2",
+    "zorunlu": true,
+    "secenekler": [
+      "Fenerbahçe",
+      "Galatasaray",
+      "Beşiktaş",
+      "Trabzonspor",
+      "Kayserispor",
+      "Malatyaspor",
+      "Göztepe"
+    ]
+  },{
+    "id": 1,
+    "title": "Doğum Yerinizi seçiniz?",
+    "type": "3",
+    "zorunlu": true,
+    "secenekler": [
+      "ADANA",
+      "MERSİN",
+      "ANTALYA",
+      "KONYA",
+      "HATAY",
+      "İSTANBUL",
+      "ELAZIĞ"
+    ]
+  },
+  {
+    "id": "10",
+    "title": "Adınız nedir?",
+    "type": "0",
+    "zorunlu": true
+  },
+  {
+    "id": "14",
+    "title": "Doğum tarihinizi seçiniz..",
+    "type": "7",
+    "zorunlu": true
+  },
+  {
+    "id": "54",
+    "title": "Kaç yaşındasınız?",
+    "type": "6",
+    "zorunlu": true
+  },
+  {
+    "id": 3,
+    "title": "Evli misiniz?",
+    "type": "1",
+    "zorunlu": true,
+    "secenekler": [
+      "evet",
+      "hayir"
+    ]
+  },
+  {
+    "id": 4,
+    "title": "Mail adresiniz nedir?",
+    "type": "4",
+    "zorunlu": true
+  },
+  {
+    "id":5,
+    "title": "Telefon numaranız nedir?",
+    "type": "5",
+    "zorunlu": true
+  },
+  {
+    "id": 6,
+    "title": "Adresiniz nedir?",
+    "type": "0",
+    "zorunlu": true
+  },
+  {
+    "id": 0,
+    "title": "Cinsiyetiniz nedir?",
+    "type": "1",
+    "zorunlu": true,
+    "secenekler": [
+      "erkek",
+      "kadın"
+    ]
+  },
+  {
+    "id": 7,
+    "title": "Website adresiniz nedir?",
+    "type": "0",
+    "zorunlu": true
+  },
+  {
+    "id": 45,
+    "title": "Sosyal medya adresleriniz nelerdir?",
+    "type": "0",
+    "zorunlu": false
+  }
+],
+"anket":[{
+  "anket_id":"1",
+  "anket_adi":"Staj Anket Deneme 1"
+}
+]
+}';
+?>
