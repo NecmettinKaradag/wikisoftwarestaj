1e553b06754da05aadb928e15961b92aefeb1da8
NecmettinKaradag <necmettink19@gmail.com>
2019-12-29 11:27:53 +0300

    project pertema

---
+++ anket2.php
@@ -0,0 +1,97 @@
+<?php
+echo '{"list":[
+
+  {
+    "id": 100,
+    "title": "Cinsiyetini seçer misin?",
+    "type": "1",
+    "zorunlu": true,
+    "secenekler": [
+      "Erkek",
+      "Kadın"
+    ]
+  },{
+    "id": 101,
+    "title": "Yaşını da seç.",
+    "type": "3",
+    "zorunlu": true,
+    "secenekler": [
+      "18 Yaş Altı",
+      "18-24",
+      "25-34",
+      "35-44",
+      "45-64",
+      "65+"
+    ]
+  },{
+    "id": 102,
+    "title": "Evden çıkmadan önce yapacağın son şey nedir?",
+    "type": "3",
+    "zorunlu": true,
+    "secenekler": [
+      "Aynaya bakar kıyafetimi son kez kontrol ederim.",
+      "Anahtarlarımı aldın mı bakarım.",
+      "Ütüyü fişte mi unuttum diye kontrol ederim."
+    ]
+  },{
+    "id":103,
+    "title": "Lütfen boşluğu doldurur musun? Aslında insanlar doğasında ........dır/dir/dur.",
+     "type": "3",
+    "zorunlu": true,
+    "secenekler": [
+      "Bencil",
+      "Aldatıcı",
+      "Sevgi dolu",
+      "Vurdumduymaz"
+    ]
+  },{
+    "id": 104,
+    "title": "Şahsi bilgilerini paylaştığın sosyal medya siteleri için ne düşünüyorsun?",
+     "type": "3",
+    "zorunlu": true,
+    "secenekler": [
+      "Zaman kaybı",
+      "İlişkilerimizi olumsuz yönde etkiliyor.",
+      "İnsanları araştırmak için bir araç bence.",
+      "Casusluk",
+      "Takılıyorum işte kim ne yapmış diye"
+    ]
+  },{
+    "id": 105,
+    "title": "Telefonunda şifre var mı?",
+    "type": "1",
+    "zorunlu": true,
+    "secenekler": [
+      "Var tabii. Telefon benim özelim.",
+      "Yok neden olsun ki?"
+    ]
+  },{
+    "id": 106,
+    "title": "Sence güvenlik kameraları çok gerekli bir alet mi?",
+     "type": "3",
+    "zorunlu": true,
+    "secenekler": [
+      "Evet imkan oldukça kullanılmalı.",
+      "Yani olmazsa olmaz değil.",
+      "Bence hiç gerek yok!"
+    ]
+  },{
+    "id": 107,
+    "title": "Son olarak, balık hafıza bile olabilirsin, ama yapmayı asla unutmayacağın şey nedir?",
+     "type": "3",
+    "zorunlu": true,
+    "secenekler": [
+      "Arabamı kilitlemek",
+      "Telefonumun tuş kilidini kapatmak",
+      "Dişlerimi fırçalamak",
+      "Evin camlarını kapatmak"
+    ]
+  }
+],
+"anket":[{
+  "anket_id":"2",
+  "anket_adi":"Onedio Anket 1"
+}
+]
+}';
+?>
